79323f38dc40ff871a10811aaf38270747585afe
Christopher Dent <christopherdent01@gmail.com>
2021-01-18 19:39:03 -0500

    installed packages and imported files to index.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> Welcome to Pomagotchi </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
